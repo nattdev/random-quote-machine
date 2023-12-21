5b4fc9c9693444ab198359ab4aef0be0155c8c97
Vanessa <110750471+nattdev@users.noreply.github.com>
2023-12-20 19:49:01 -0500

    Add onClick handler

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,6 +1,6 @@
-function Button() {
+function Button({onClick}) {
     return (
-        <button id="new-quote" className="new-quote btn btn-primary">New Quote</button>
+        <button id="new-quote" className="new-quote btn btn-primary" onClick={onClick}>New Quote</button>
     );
 }
 
