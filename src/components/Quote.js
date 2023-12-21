b6df2d69f558a0cb1f39b0879dc88e7e81e2aec2
Vanessa <110750471+nattdev@users.noreply.github.com>
2023-12-20 19:51:07 -0500

    Update text content dynamically based on current state

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,10 +1,10 @@
-function Quote() {
+function Quote({text, name}) {
     return (
         <div className="quote-description bg-light">
             <blockquote className="fs-1 text-center blockquote">
-            <i className="bi bi-quote"></i><span className="d-inline-block" id="text">Limitations live only in our minds. But if we use our imaginations, our possibilities become limitless.</span>
+            <i className="bi bi-quote"></i><span className="d-inline-block" id="text">{text}</span>
             </blockquote>
-            <p className="author text-end fs-6"><i className="bi bi-dash"></i><span id="author">Jamie Paolinetti</span></p>
+            <p className="author text-end fs-6"><i className="bi bi-dash"></i><span id="author">{name}</span></p>
         </div>
     );
 }
